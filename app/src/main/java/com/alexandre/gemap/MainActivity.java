b354c4d0707dc3706a14e7ba09b9f52ff49f97fe
Alexandredev4800 <alexandre38alexandre1@gmail.com>
2026-04-19 00:42:07 -0300

    Add files via upload

---
+++ app/src/main/java/com/alexandre/gemap/MainActivity.java
@@ -0,0 +1,11 @@
+package com.alexandre.gemap;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Seu código do assistente de voz será compilado aqui
+    }
+}
